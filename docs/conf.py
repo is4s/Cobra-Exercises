4de83c4090586243c74a45f06997aaf613f59d3c
Daniel Carson <danielcarson271@gmail.com>
2026-03-23 09:51:35 -0400

    update exclusion
    carried over from last project

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ nitpicky = True
 linkcheck_allowed_redirects = {
     # All redirections to the sign-in page will be counted as "working".
     r'.*git.aspn.us.*': r'https://git.aspn.us/users/sign_in',
-    r'.*pages\.aspn\.us(?!/pntos-python/).*': r'https://git.aspn.us/users/sign_in',
+    r'.*pages\.aspn\.us(?!/pntos-python-exercises/).*': r'https://git.aspn.us/users/sign_in',
     'https://www.pntOS.com': 'https://www.pntos.com/',
 }
 # Ignore line number anchors (e.g. #L12), since linkcheck gives false positives for these.
