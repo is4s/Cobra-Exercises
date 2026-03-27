d0abea514e983d7b40d8379ff150024345b1a69b
bhowell <braden.howell@is4s.com>
2026-03-27 16:14:24 -0400

    add integration test to ci

--- test/integration_test.py
+++ test/integration_test.py
@@ -10,3 +10,13 @@ def test_zupt_stub() -> None:
 def test_zupt_sol() -> None:
     # just make sure the app can run to completion
     run_pntos_with_log_transport(Path('.details/zupt_exc_ans/zupt_app.py'))
+
+
+def test_pos_baro_stub() -> None:
+    # just make sure the app can run to completion
+    run_pntos_with_log_transport(Path('exercises/new_app_exercise/pos_baro_app.py'))
+
+
+def test_pos_baro_sol() -> None:
+    # just make sure the app can run to completion
+    run_pntos_with_log_transport(Path('.details/new_app_exc_ans/pos_baro_app.py'))
