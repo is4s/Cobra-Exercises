e2b84f47ed45153a17acb499f8e586ede5126831
Daniel Marietta <daniel.marietta@is4s.com>
2026-04-19 15:23:14 -0400

    Add app patch

--- util/generate_patches.sh
+++ util/generate_patches.sh
@@ -5,3 +5,4 @@
 
 git diff --no-index exercises/zupt_exercise/zupt_app.py .details/zupt_exc_ans/zupt_app.py > util/zupt_app.patch
 git diff --no-index exercises/new_app_exercise/pos_baro_app.py .details/new_app_exc_ans/pos_baro_app.py > util/pos_baro_app.patch
+git diff --no-index exercises/direction_processor_exercise/direction_app.py .details/direction_exc_ans/direction_app.py > util/direction_app.patch
